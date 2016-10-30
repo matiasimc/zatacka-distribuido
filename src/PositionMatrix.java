8b1de88eccccfe52a57b363f38a6b7a25facdae1
Matias Meneses <matias.imc@gmail.com>
2016-10-30 10:44:51 -0300

    Matriz de posicion

--- src/PositionMatrix.java
+++ src/PositionMatrix.java
@@ -1,5 +1,41 @@
-
+class CollisionException extends Exception {}
 
 public class PositionMatrix {
+	/*
+	 * matrix[x][y] = clientID indica si un cliente paso
+	 * por esa posicion, y 0 si no ha pasado nadie
+	 */
+	private static int WIDTH = 800;
+	private static int HEIGHT = 800;
+	private int[][] matrix;
 	
+	public PositionMatrix() {
+		matrix = new int[WIDTH][HEIGHT];
+	}
+	
+	public int checkCircle(int cx, int cy, int r) {
+		for (int i = cx-r; i < cx+r; i++) {
+			for (int j = cy-r; j < cy+r; j++) {
+				if (Math.pow((i-cx),2) + Math.pow((j-cy),2) < Math.pow(r,2)) {
+					return matrix[i][j];
+				}
+			}
+		}
+		return 0;
+	}
+	
+	public void fillCircle(int cx, int cy, int r, int id) {
+		for (int i = cx-r; i < cx+r; i++) {
+			for (int j = cy-r; j < cy+r; j++) {
+				if (Math.pow((i-cx),2) + Math.pow((j-cy),2) < Math.pow(r,2)) {
+					matrix[i][j] = id;
+				}
+			}
+		}
+	}
+	
+	public void fill(int x, int y, int id) throws CollisionException {
+		if (checkCircle(x,y, Point.dHip/2) != 0) throw new CollisionException();
+		else fillCircle(x,y, Point.dHip/2, id);
+	}
 }
